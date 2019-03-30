441429e4a59a797e4af9314b3bf7be1923821278
StoyanovBG <yavor377@gmail.com>
2019-03-30 12:32:07 +0200

    Adding View/Display

---
+++ DivnaShoes/Views/Display.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DivnaShoes.Views
+{
+    public class Display
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Number { get; set; }
+
+
+        public Display()
+        {
+            Console.WriteLine("Insert:" + System.Environment.NewLine +
+                "---------------------------------" + System.Environment.NewLine +
+                "First name: " + System.Environment.NewLine +
+                "Last name:" + System.Environment.NewLine +               
+                "---------------------------------");
+            getValues();
+        }
+
+        public void getValues()
+        {
+            First_Name = Console.ReadLine();
+            Last_Name = Console.ReadLine();
+            
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(First_Name + " - " + Last_Name);
+            Console.WriteLine("---------------------------------");
+        }
+
+
+
+
+    }
+}

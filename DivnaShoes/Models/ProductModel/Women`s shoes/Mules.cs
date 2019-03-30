b1950004dbaf1af3ee11767cd68c2a1fbde78ecc
StoyanovBG <yavor377@gmail.com>
2019-03-30 12:29:35 +0200

    Deleting View

--- DivnaShoes/Models/ProductModel/Women`s shoes/Mules.cs
+++ DivnaShoes/Models/ProductModel/Women`s shoes/Mules.cs
@@ -7,7 +7,7 @@ namespace DivnaShoes.Models.ProductModel.Women_s_shoes
     public class Mules : Product
     {
         static string name = "Mules";
-        const decimal price = 420;
+        const decimal price = 425;
         static string brand = "Tory Burch";
 
         public Mules(int Size) : base(name, Size, price, brand)

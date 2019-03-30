3d5f11455eceb0d1579d5b6d326e3ac9ba9dc2a8
Svetoslav Konakov <svetoslav.konakov@gmail.com>
2019-03-30 10:50:27 +0200

    Creating ProductModel

--- DivnaShoes/Models/ProductModel/Product.cs
+++ DivnaShoes/Models/ProductModel/Product.cs
@@ -4,7 +4,71 @@ using System.Text;
 
 namespace DivnaShoes.Models.ProductModel
 {
-    class Product
+    public abstract class Product
     {
+        public string name;
+        public int size;
+        public decimal price;
+        public string brand { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (name == null)
+                {
+                    throw new InvalidOperationException("The Product name cannnot be null");
+                }
+                this.name = value;
+            }
+
+        }
+
+        public int Size
+        {
+            get
+            {
+                return this.size;
+            }
+            set
+            {
+                if (size <= 0)
+                {
+                    throw new InvalidOperationException("The Product size cannot be 0 or below");
+                }
+                this.size = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (price < 0)
+                {
+                    throw new InvalidOperationException("The Price cannot be negative number");
+                }
+                this.price = value;
+            }
+
+        }
+      
+
+        public Product(string Name, int Size, decimal Price,string brand)
+        {
+            this.name = Name;
+            this.size = Size;
+            this.price = Price;
+            this.brand = brand;
+
+        }
     }
 }
